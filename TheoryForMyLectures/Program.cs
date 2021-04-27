5a4f7b69b9a7873f49e270096bd272f21e89d17d
Georgi Kolev <61197544+Jorjo77@users.noreply.github.com>
2021-04-27 20:14:35 +0300

    TaskUpdate

--- TheoryForMyLectures/Program.cs
+++ TheoryForMyLectures/Program.cs
@@ -6,7 +6,10 @@ namespace TheoryForMyLectures
     {
         static void Main(string[] args)
         {
-            //Каква е разликата между Dependency Injection и Dependency Inversion (много любим въпрос по интервюта)   - 
+            //Каква е разликата между Dependency Injection и Dependency Inversion (много любим въпрос по интервюта)   - Dependency Inversion e принципа, 
+            //който казва че зависимостите трябва да получаваме отвън, а Dependency Injection е начина по който го правим, това е конкретната имплементация на
+            //Dependency Inversion!!! Dependency Inversion най-много се използва за тестване, ако нямаме Dependency Injection е много трудно да си тестваме кода,
+            //това е най-голямата полза, а също и това че прави кода по-абстрактен и по-лесен са екстендване впоследствие.
 
             //Какво е Poor man's Dependency Inversion - 
 
