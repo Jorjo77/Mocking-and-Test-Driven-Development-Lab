d17f8c757ae1f4275558ba71b739d129e64f1810
Georgi Kolev <tuvkontrol55@gmail.com>
2021-04-27 16:48:27 +0300

    Add project files.

---
+++ TheoryForMyLectures/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TheoryForMyLectures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Каква е разликата между Dependency Injection и Dependency Inversion (много любим въпрос по интервюта)  
+
+            //Mocking - 
+        }
+    }
+}
